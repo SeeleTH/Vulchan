9cbc83ebd771a1a0a10d699aa005836c9bfb12b4
SeeleTH <namo_95@hotmail.com>
2016-09-22 06:34:28 +0900

    add window code (prepare for starting)

---
+++ source/Vulchan.cpp
@@ -0,0 +1,55 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+	case WM_CLOSE: {
+		PostQuitMessage(0);
+		break;
+	}
+	default: {
+		break;
+	}
+	}
+
+	// a pass-through for now. We will return to this callback
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	WNDCLASSEX windowClass = {};
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+	windowClass.lpfnWndProc = WindowProc;
+	windowClass.hInstance = hInstance;
+	windowClass.lpszClassName = "VulkanWindowClass";
+	RegisterClassEx(&windowClass);
+
+	HWND windowHandle = CreateWindowEx(NULL, "VulkanWindowClass", "Vulchan",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		100,
+		100,
+		800, 
+		600,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	MSG msg;
+	bool done = false;
+	while (!done) {
+		PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE);
+		if (msg.message == WM_QUIT) {
+			done = true;
+		}
+		else {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		RedrawWindow(windowHandle, NULL, NULL, RDW_INTERNALPAINT);
+	}
+
+	return msg.wParam;
+}
